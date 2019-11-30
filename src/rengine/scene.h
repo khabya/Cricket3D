0c5fb7c7ec98ffcd5f829b74e03344a758703876
Roshan Lamichhane <33913826+roshanlam@users.noreply.github.com>
2019-11-30 17:46:02 -0500

    Create scene.h

---
+++ src/rengine/scene.h
@@ -0,0 +1,22 @@
+#ifndef SCENE_H
+#define SCENE_H
+#include "model.h"
+#include "camera.h"
+#include <glm/glm.hpp>
+
+struct Light {
+  glm::vec3 position{0.0f, 1.0f, 3.0f};
+  glm::vec3 color{1.0f};
+
+  float intensity = 1.0f;
+  float ambientStrength = 0.3f;
+  float specularStrength = 0.5f;
+};
+
+struct Scene {
+  Camera mainCamera;
+  Light mainLight;
+  Model mainModel;
+};
+
+#endif
