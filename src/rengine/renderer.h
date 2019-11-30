16b1574a5e1d55ea371e7ed173ca39ce1fc48e96
Roshan Lamichhane <33913826+roshanlam@users.noreply.github.com>
2019-11-30 17:45:00 -0500

    Create renderer.h

---
+++ src/rengine/renderer.h
@@ -0,0 +1,30 @@
+#ifndef RENDERER_H
+#define RENDERER_H
+#include "scene.h"
+#include <memory>
+
+class Renderer {
+
+public:
+  ~Renderer();
+  static Renderer& getInstance(){
+    static Renderer instance;
+    return instance;
+  }
+
+  void prepare();
+  void cleanUp();
+  void render();
+  void resize(int width, int height);
+  void setActiveScene(Scene &scene);
+
+  glm::vec3 mBackgroundColor{0.2f};
+  bool wireframeMode = false;
+  
+private:
+  Renderer();
+  Scene *mActiveScene = nullptr;
+
+};
+
+#endif
