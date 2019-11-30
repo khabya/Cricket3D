0db5d8b0dd52da8902df75ba02717b4db56334d8
Roshan Lamichhane <33913826+roshanlam@users.noreply.github.com>
2019-11-30 17:39:14 -0500

    Create camera.cpp

---
+++ src/rengine/camera.cpp
@@ -0,0 +1,46 @@
+#include "camera.h"
+glm::vec3 pos(0.0f, 0.0f, 2.5f);
+const glm::vec3 forward(0.0f, 0.0f, -1.0f);
+const glm::vec3 up(0.0f, 1.0f, 0.0f);
+const glm::vec3 right(1.0f, 0.0f, 0.0f);
+
+void Camera::move(CameraDirection dir, float speed){
+  switch (dir) {
+      case FORWARD:
+        pos += forward * speed;
+        break;
+      case BACKWARD:
+        pos -= forward * speed;
+        break;
+      case UPWARD:
+        pos += up * speed;
+        break;
+      case DOWNWARD:
+        pos -= up * speed;
+        break;
+      case RIGHTWARD:
+        pos += right * speed;
+        break;
+      case LEFTWARD:
+        pos -= right * speed;
+        break;
+      default:
+        break;
+  }
+}  
+
+void Camera::reset(){
+    pos = glm::vec3(0.0f, 0.0f, 2.5f);
+}
+
+glm::vec3 Camera::getTarget(){
+  return pos + forward;
+}
+
+glm::vec3 Camera::getUp(){
+  return up;
+}
+
+glm::vec3 Camera::getPosition(){
+  return pos;
+}
