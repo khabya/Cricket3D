088bafb9abd4634b6bdbc04bd788bf3136bf039c
Roshan Lamichhane <33913826+roshanlam@users.noreply.github.com>
2019-11-30 17:39:50 -0500

    Create camera.h

---
+++ src/rengine/camera.h
@@ -0,0 +1,24 @@
+#ifndef CAMERA_H
+#define CAMERA_H
+#include <glm/glm.hpp>
+
+enum CameraDirection {
+  FORWARD,
+  BACKWARD,
+  DOWNWARD,
+  UPWARD,
+  RIGHTWARD,
+  LEFTWARD
+};  
+
+class Camera {
+public:
+  void move(CameraDirection dir, float speed);
+  
+  void reset();
+  glm::vec3 getTarget();
+  glm::vec3 getUp();
+  glm::vec3 getPosition();
+};
+
+#endif
