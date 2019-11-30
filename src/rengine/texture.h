cb90f18e6b52db4d32b91a1f5b1227510576a8f4
Roshan Lamichhane <33913826+roshanlam@users.noreply.github.com>
2019-11-30 18:02:34 -0500

    Create texture.h

---
+++ src/rengine/texture.h
@@ -0,0 +1,28 @@
+#ifndef TEXTURE_H
+#define TEXTURE_H
+#include <GL/glew.h>
+#include <string>
+#include <unordered_map>
+
+enum TextureType {
+  DIFFUSE,
+  SPECULAR
+};
+
+class Texture {
+private:  
+  GLuint mId;
+  TextureType mType;
+  std::string mPath;
+  static std::unordered_map<std::string, GLuint> mTexturePool;
+
+public:
+  Texture(std::string path, TextureType type);
+  void load();
+  static void release();
+  GLuint getId();
+  TextureType getType();
+  std::string getPath();
+};
+
+#endif
