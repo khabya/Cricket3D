2e51d4598bd8588ca35a1b69b3e9c3aff205d014
Roshan Lamichhane <33913826+roshanlam@users.noreply.github.com>
2019-11-30 17:42:48 -0500

    Create model.h

---
+++ src/rengine/model.h
@@ -0,0 +1,35 @@
+#ifndef MODEL_H
+#define MODEL_H
+#include "mesh.h"
+#include <vector>
+#include <string>
+#include <assimp/Importer.hpp>
+#include <assimp/scene.h>
+#include <assimp/postprocess.h>
+#include <assimp/config.h>
+#include <glm/glm.hpp>
+#include <glm/gtc/matrix_transform.hpp>
+
+
+class Model {
+
+public:
+  Model();
+  ~Model();
+
+  std::vector<Mesh> mMeshes;
+  void rotateHorizontally(float angle);
+  void rotateVertically(float angle);
+  void loadFromFilename(std::string filename);
+  void resetModelMatrix();
+  glm::mat4 getModelMatrix();
+  
+private:
+  std::string mCurrentFolder;
+  glm::mat4 mModelMatrix;
+  Mesh loadMesh(const aiMesh*, const aiScene*);
+  void iterateNode(const aiNode*, const aiScene*);
+  std::vector<Texture> getTextures(const aiMaterial *material, TextureType textureType);
+};
+
+#endif
