4198b83eeb41322ad1d323ce38f65d0ed577a3ac
Roshan Lamichhane <33913826+roshanlam@users.noreply.github.com>
2019-11-30 17:40:22 -0500

    Create mesh.h

---
+++ src/rengine/mesh.h
@@ -0,0 +1,26 @@
+#ifndef MESH_H
+#define MESH_H
+#include <vector>
+#include <glm/glm.hpp>
+#include "texture.h"
+
+struct Vertex {
+  glm::vec3 pos;
+  glm::vec4 color;
+  glm::vec2 texCoords;
+  glm::vec3 nor;
+};
+
+struct Mesh {
+
+  std::vector<Vertex> mVertices;
+  std::vector<GLuint> mIndices;
+  std::vector<Texture> mTextures;
+  
+  GLuint mVAO;
+  GLuint mVBO;
+  GLuint mEBO;
+  
+};
+
+#endif
