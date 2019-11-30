461b646ff3a8bbbfd1e42a8afb5409f6bf89ecd4
Roshan Lamichhane <33913826+roshanlam@users.noreply.github.com>
2019-11-30 17:14:33 -0500

    Create mainwindow.h

---
+++ src/gui/mainwindow.h
@@ -0,0 +1,32 @@
+#ifndef MAINWINDOW_H
+#define MAINWINDOW_H
+
+#include <QMainWindow>
+
+namespace Ui {
+class MainWindow;
+}
+
+class MainWindow : public QMainWindow{
+    Q_OBJECT
+
+public:
+    explicit MainWindow(QWidget *parent = 0);
+    ~MainWindow();
+
+private slots:
+    void on_actionOpen_triggered();
+    void on_actionReset_Position_triggered();
+    void on_actionSettings_triggered(bool checked);
+    void on_intensitySlider_sliderMoved(int position);
+    void on_ambientSlider_sliderMoved(int position);
+    void on_specularSlider_sliderMoved(int position);
+    void on_lightColorButton_clicked();
+    void on_bgColorButton_clicked();
+    void on_actionWireframe_triggered(bool checked);
+
+private:
+    Ui::MainWindow *ui;
+};
+
+#endif // MAINWINDOW_H
